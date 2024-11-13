bcfb30ce3ba43830753564f1d8ff6df870077c03
Sunilparida98 <paridasunil65@gmail.com>
2024-11-13 10:32:45 +0530

    modify somthing

--- script.js
+++ script.js
@@ -21,3 +21,62 @@
       });
     }); // end DOM ready
   })(jQuery); // end jQuery
+
+
+
+  // show sidebar
+
+
+  const showSidebar = (toggleId, sidebarId) =>{
+   const toggle = document.getElementById(toggleId),
+   sidebar = document.getElementById(sidebarId)
+  
+
+   if(toggle && sidebar){
+       toggle.addEventListener('click', ()=>{
+           /* Show sidebar */
+           sidebar.classList.toggle('show-sidebar')
+          
+       })
+   }
+}
+showSidebar(header-toggle,sidebar)
+// const hiden_navbar = document.getElementById(header-toggle);
+
+
+
+
+//feedback
+ document.addEventListener('DOMContentLoaded', () => {
+  let rating = 0;
+
+  const ratingStars = document.querySelectorAll('#rating-stars .star');
+  const form = document.getElementById('feedback-form');
+
+  ratingStars.forEach((star) => {
+    star.addEventListener('click', () => {
+      rating = star.getAttribute('data-star');
+      updateStarColors(rating);
+    });
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name1').value;
+    const email = document.getElementById('email1').value;
+    const feedback = document.getElementById('feedback1').value;
+
+    console.log(`Name: ${name}, Email: ${email}, Rating: ${rating}, Feedback: ${feedback}`);
+    alert("Thank you for your feedback!");
+  });
+
+  function updateStarColors(rating) {
+    ratingStars.forEach((star, index) => {
+      if (index < rating) {
+        star.classList.add('selected');
+      } else {
+        star.classList.remove('selected');
+      }
+    });
+  }
+});
